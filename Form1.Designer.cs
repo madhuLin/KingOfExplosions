22b9380e466e8eb42552d8d862dc88008154496a
a110222040 <a110222040@mail.shu.edu.tw>
2023-11-29 18:47:49 +0800

    做了一些功能

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2P = new System.Windows.Forms.PictureBox();
+            this.pictureBox1P = new System.Windows.Forms.PictureBox();
+            this.buttonFocus = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2P)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1P)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox2P);
+            this.panel1.Controls.Add(this.pictureBox1P);
+            this.panel1.Location = new System.Drawing.Point(69, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(500, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox2P
+            // 
+            this.pictureBox2P.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2P.Image")));
+            this.pictureBox2P.Location = new System.Drawing.Point(451, 449);
+            this.pictureBox2P.Name = "pictureBox2P";
+            this.pictureBox2P.Size = new System.Drawing.Size(46, 48);
+            this.pictureBox2P.TabIndex = 1;
+            this.pictureBox2P.TabStop = false;
+            // 
+            // pictureBox1P
+            // 
+            this.pictureBox1P.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1P.Name = "pictureBox1P";
+            this.pictureBox1P.Size = new System.Drawing.Size(45, 54);
+            this.pictureBox1P.TabIndex = 0;
+            this.pictureBox1P.TabStop = false;
+            // 
+            // buttonFocus
+            // 
+            this.buttonFocus.Location = new System.Drawing.Point(594, 499);
+            this.buttonFocus.Name = "buttonFocus";
+            this.buttonFocus.Size = new System.Drawing.Size(23, 23);
+            this.buttonFocus.TabIndex = 2;
+            this.buttonFocus.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(608, 28);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 220);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(917, 566);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonFocus);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2P)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1P)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2P;
+        private System.Windows.Forms.PictureBox pictureBox1P;
+        private System.Windows.Forms.Button buttonFocus;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,75 @@ namespace KingOfExplosions
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true; // 啟用按鍵事件的 Form 預覽
+        //buttonFocus.Select();
+    }
+        string appPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\"));
+        bool play = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1P.Image = Image.FromFile(appPath + @"img\p1.jpg");
+            pictureBox2P.Image = Image.FromFile(appPath + @"img\p2.png");
+            PictureBox[][] pictureBoxes = new PictureBox[10][];
+
+            // 初始化 PictureBox 並添加到控制項
+            for (int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i] = new PictureBox[10]; // 創建每一行的 PictureBox 陣列
+
+                for (int j = 0; j < pictureBoxes[i].Length; j++)
+                {
+                    PictureBox pictureBox = new PictureBox();
+                    pictureBoxes[i][j] = pictureBox;
+
+                    // 設定 PictureBox 的位置和大小
+                    int pictureBoxWidth = 50; // 設定寬度
+                    int pictureBoxHeight = 50; // 設定高度
+                    int pictureBoxSpacing = 0; // 設定 PictureBox 之間的間距
+                    //pictureBox.Image = KingOfExplosions.Properties.Resources.floor;
+                    pictureBox.Image = Image.FromFile(appPath + @"img\floor.png");
+
+                    pictureBox.SetBounds(j * (pictureBoxWidth + pictureBoxSpacing), i * (pictureBoxHeight + pictureBoxSpacing), pictureBoxWidth, pictureBoxHeight);
+
+                    // 將 PictureBox 添加到 Panel 的控制項集合中
+                    panel1.Controls.Add(pictureBox);
+                }
+            }
         }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            //if (play == false) return;
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    if(pictureBox1P.Left >= 0) pictureBox1P.Left -= 5; //左移                    
+                    break;
+                case Keys.D:
+                    if (pictureBox1P.Right < panel1.Width) pictureBox1P.Left += 5; //右移
+                    break;
+                case Keys.W:
+                    if (pictureBox1P.Top >= 0) pictureBox1P.Top -= 5; //上移
+                    break;
+                case Keys.S:
+                    if(pictureBox1P.Bottom < panel1.Height) pictureBox1P.Top += 5; //下移
+                    break;
+                case Keys.Space:
+                    PictureBox pictureBox = new PictureBox();
+                    
+                    int pictureBoxWidth = 45; // 設定寬度
+                    int pictureBoxHeight = 45; // 設定高度
+                    listBox1.Items.Add(pictureBox1P.Left +"   "+pictureBox1P.Top);
+                    pictureBox.Image = Image.FromFile(appPath + @"\img\bom.png");
+                    pictureBox.SetBounds(pictureBox1P.Left, pictureBox1P.Top, pictureBoxWidth, pictureBoxHeight);
+                    panel1.Controls.Add(pictureBox);
+                    break;
+            }
+            buttonFocus.Select();
+
+        }
+        
     }
 }
